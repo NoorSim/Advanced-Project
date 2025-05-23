12a2d2754871cefcd9babf747c80025b185ca45c
NoorSim <nurafni.simamora@gmail.com>
2025-05-23 20:30:12 +0300

    dark mode done

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
-//
-//
-//
+let isModalOpen = false;
+let contrastToggle = false;
+
+function toggleContrast() {
+    contrastToggle = !contrastToggle;
+    if (contrastToggle) {
+       document.body.classList += " dark-theme" 
+    }
+    else {
+       document.body.classList.remove("dark-theme")
+    }
+}
 
 function contact(event) {
   event.preventDefault();
@@ -26,7 +35,7 @@ function contact(event) {
     });
 }
 
-let isModalOpen = false;
+
 function toggleModal() {
     if (isModalOpen) {
         isModalOpen = false;
